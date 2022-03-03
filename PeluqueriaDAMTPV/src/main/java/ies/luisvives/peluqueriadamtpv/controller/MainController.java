38dd83ba23428c1dc9a6d340fd3b604558d89110
Federico Toledo <federicotoledobaeza@gmail.com>
2022-03-03 20:56:20 +0100

    Menu buttons changes and animated, CSS first changes

--- PeluqueriaDAMTPV/src/main/java/ies/luisvives/peluqueriadamtpv/controller/MainController.java
+++ PeluqueriaDAMTPV/src/main/java/ies/luisvives/peluqueriadamtpv/controller/MainController.java
@@ -2,18 +2,24 @@ package ies.luisvives.peluqueriadamtpv.controller;
 
 import ies.luisvives.peluqueriadamtpv.model.UserConfiguration;
 import ies.luisvives.peluqueriadamtpv.utils.Util;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
+import java.util.List;
 import java.util.Objects;
 
 public class MainController {
+    TranslateTransition toggleTranlate;
     @FXML
     public SplitPane mainPane;
     @FXML
     StackPane includedViewAppointments, includedViewUsers, includedViewServices, includedViewReports, includedViewSettings;
+    @FXML
+    ToggleGroup sidemenutogglebuttons;
     @FXML
     ToggleButton mainViewSideMenuButtonAppointments, mainViewSideMenuButtonUsers, mainViewSideMenuButtonServices, mainViewSideMenuButtonReports, mainViewSideMenuButtonSettings;
     @FXML
@@ -33,84 +39,81 @@ public class MainController {
                         UserConfiguration.getInstance().getActualTheme().toLowerCase()
                         + ".css")).toExternalForm());
     }
-
     @FXML
-    public void onMenuItemAppointmentsMouseClicked() {
-        includedViewAppointments.setVisible(true);
-        includedViewAppointments.setDisable(false);
-        includedViewUsers.setVisible(false);
-        includedViewUsers.setDisable(true);
-        includedViewServices.setVisible(false);
-        includedViewServices.setDisable(true);
-        includedViewReports.setVisible(false);
-        includedViewReports.setDisable(true);
-        includedViewSettings.setVisible(false);
-        includedViewSettings.setDisable(true);
-        search_box.setVisible(true);
-
+    public void onMenuItemSelected(){
+        if (mainViewSideMenuButtonAppointments.isSelected()){
+            includedViewAppointments.setVisible(true);
+            includedViewAppointments.setDisable(false);
+            search_box.setVisible(true);
+            mainViewSideMenuButtonAppointments.setDisable(true);
+            animateToggleButton(mainViewSideMenuButtonAppointments);
+        }else{
+            includedViewAppointments.setVisible(false);
+            includedViewAppointments.setDisable(true);
+            mainViewSideMenuButtonAppointments.setDisable(false);
+            animateToggleButton(mainViewSideMenuButtonAppointments);
+        }
+        if (mainViewSideMenuButtonUsers.isSelected()){
+            includedViewUsers.setVisible(true);
+            includedViewUsers.setDisable(false);
+            search_box.setVisible(true);
+            mainViewSideMenuButtonUsers.setDisable(true);
+            animateToggleButton(mainViewSideMenuButtonUsers);
+        }else{
+            includedViewUsers.setVisible(false);
+            includedViewUsers.setDisable(true);
+            mainViewSideMenuButtonUsers.setDisable(false);
+            animateToggleButton(mainViewSideMenuButtonUsers);
+        }
+        if (mainViewSideMenuButtonServices.isSelected()){
+            includedViewServices.setVisible(true);
+            includedViewServices.setDisable(false);
+            search_box.setVisible(true);
+            mainViewSideMenuButtonServices.setDisable(true);
+            animateToggleButton(mainViewSideMenuButtonServices);
+        }else{
+            includedViewServices.setVisible(false);
+            includedViewServices.setDisable(true);
+            mainViewSideMenuButtonServices.setDisable(false);
+            animateToggleButton(mainViewSideMenuButtonServices);
+        }
+        if (mainViewSideMenuButtonReports.isSelected()){
+            includedViewReports.setVisible(true);
+            includedViewReports.setDisable(false);
+            search_box.setVisible(false);
+            mainViewSideMenuButtonReports.setDisable(true);
+            animateToggleButton(mainViewSideMenuButtonReports);
+        }else{
+            includedViewReports.setVisible(false);
+            includedViewReports.setDisable(true);
+            mainViewSideMenuButtonReports.setDisable(false);
+            animateToggleButton(mainViewSideMenuButtonReports);
+        }
+        if (mainViewSideMenuButtonSettings.isSelected()){
+            includedViewSettings.setVisible(true);
+            includedViewSettings.setDisable(false);
+            search_box.setVisible(false);
+            mainViewSideMenuButtonSettings.setDisable(true);
+            animateToggleButton(mainViewSideMenuButtonSettings);
+        }else{
+            includedViewSettings.setVisible(false);
+            includedViewSettings.setDisable(true);
+            mainViewSideMenuButtonSettings.setDisable(false);
+            animateToggleButton(mainViewSideMenuButtonSettings);
+        }
     }
-
-    @FXML
-    public void onMenuItemUsersMouseClicked() {
-        includedViewAppointments.setVisible(false);
-        includedViewAppointments.setDisable(true);
-        includedViewUsers.setVisible(true);
-        includedViewUsers.setDisable(false);
-        includedViewServices.setVisible(false);
-        includedViewServices.setDisable(true);
-        includedViewReports.setVisible(false);
-        includedViewReports.setDisable(true);
-        includedViewSettings.setVisible(false);
-        includedViewSettings.setDisable(true);
-        search_box.setVisible(true);
-
+    public void animateToggleButton(ToggleButton toggleButton){
+        toggleTranlate = new TranslateTransition(Duration.millis(100),toggleButton);
+        if(toggleButton.isSelected()){
+        toggleTranlate.setFromX(toggleButton.getTranslateX());
+        toggleTranlate.setToX(toggleButton.getTranslateX()+25);}
+        else{
+           // toggleTranlate.setDuration(Duration.millis(1));
+            toggleTranlate.setFromX(toggleButton.getLayoutX());
+            toggleTranlate.setToX(toggleButton.getLayoutX());
+        }
+        toggleTranlate.play();
     }
-
-    @FXML
-    public void onMenuItemServicesMouseClicked() {
-        includedViewAppointments.setVisible(false);
-        includedViewAppointments.setDisable(true);
-        includedViewUsers.setVisible(false);
-        includedViewUsers.setDisable(true);
-        includedViewServices.setVisible(true);
-        includedViewServices.setDisable(false);
-        includedViewReports.setVisible(false);
-        includedViewReports.setDisable(true);
-        includedViewSettings.setVisible(false);
-        includedViewSettings.setDisable(true);
-        search_box.setVisible(true);
-
-    }
-
-    @FXML
-    public void onMenuItemReportsMouseClicked() {
-        includedViewAppointments.setVisible(false);
-        includedViewAppointments.setDisable(true);
-        includedViewUsers.setVisible(false);
-        includedViewUsers.setDisable(true);
-        includedViewServices.setVisible(false);
-        includedViewServices.setDisable(true);
-        includedViewReports.setVisible(true);
-        includedViewReports.setDisable(false);
-        includedViewSettings.setVisible(false);
-        includedViewSettings.setDisable(true);
-        search_box.setVisible(false);
-    }
-
-    @FXML
-    public void onMenuItemSettingsMouseClicked() {
-        includedViewAppointments.setVisible(false);
-        includedViewAppointments.setDisable(true);
-        includedViewUsers.setVisible(false);
-        includedViewUsers.setDisable(true);
-        includedViewServices.setVisible(false);
-        includedViewServices.setDisable(true);
-        includedViewReports.setVisible(false);
-        includedViewReports.setDisable(true);
-        includedViewSettings.setVisible(true);
-        includedViewSettings.setDisable(false);
-    }
-
     @FXML
     public void onSearchButtonClick() {
         includedViewAppointmentsController.setSearchQuery(search_field.getText());
