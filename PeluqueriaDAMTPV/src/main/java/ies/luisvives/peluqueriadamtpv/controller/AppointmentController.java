af932f6613d84a5d8797a1b6cde2ac87bf4a0df9
Madirex <madirexland@gmail.com>
2022-03-04 21:24:44 +0100

    Added service information and username field to appointment view

--- PeluqueriaDAMTPV/src/main/java/ies/luisvives/peluqueriadamtpv/controller/AppointmentController.java
+++ PeluqueriaDAMTPV/src/main/java/ies/luisvives/peluqueriadamtpv/controller/AppointmentController.java
@@ -1,12 +1,20 @@
 package ies.luisvives.peluqueriadamtpv.controller;
 
+import ies.luisvives.peluqueriadamtpv.model.Service;
+import ies.luisvives.peluqueriadamtpv.model.TableEntity;
+import ies.luisvives.peluqueriadamtpv.restcontroller.APIRestConfig;
 import ies.luisvives.peluqueriadamtpv.utils.Util;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import retrofit2.Response;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 public class AppointmentController implements BaseController{
@@ -27,6 +35,13 @@ public class AppointmentController implements BaseController{
     @FXML
     private HourViewController hourViewController;
 
+    @FXML
+    private Label labelService;
+
+    private ObservableList<Service> services;
+
+    private int actualServiceSelected = 0;
+
     public AppointmentController() {
     }
 
@@ -36,6 +51,48 @@ public class AppointmentController implements BaseController{
         tableViewController.setEntityForTable(TableViewController.APPOINTMENT);
         calendarViewController.setTableViewController(tableViewController);
         hourViewController.setTableViewController(tableViewController);
+        initServices();
+    }
+
+    private void initServices() {
+        Response<List<Service>> response = null;
+        try {
+            response = APIRestConfig.getServicesService().serviceGetAll().execute();
+            if (response.body() != null) {
+                services = FXCollections.observableArrayList(response.body());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        updateService();
+    }
+
+    /**
+     * Update the service data
+     * (This method establishes first service by default if exists)
+     */
+    private void updateService() {
+        if (!services.isEmpty()){
+            labelService.setText(String.valueOf(services.get(actualServiceSelected).getName()));
+        }else{
+            labelService.setText(Util.getString("text.noServices"));
+        }
+    }
+
+    @FXML
+    protected void prevServiceAction(){
+        if (!services.isEmpty() && actualServiceSelected > 0){
+            actualServiceSelected -=1;
+        }
+        updateService();
+    }
+    @FXML
+    protected void nextServiceAction(){
+        if (!services.isEmpty() && actualServiceSelected < services.size() - 1){
+            actualServiceSelected +=1;
+        }
+        updateService();
     }
 
     @FXML
